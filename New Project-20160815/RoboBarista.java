0bd1617906754e0acfcba15fb1664d5bd30b93f5
Heather Conklin <heath3conk@users.noreply.github.com>
2016-08-15 17:47:57 -0500

    Update RoboBarista.java

--- New Project-20160815/RoboBarista.java
+++ New Project-20160815/RoboBarista.java
@@ -1,6 +1,9 @@
 public class RoboBarista{
+     
+     // this is where all the interaction with the user will happen
 
      public static void main(String []args){
         System.out.println("Hello World");
      }
+
 }
